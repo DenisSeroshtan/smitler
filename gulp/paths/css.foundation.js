ab08b52d0d491da88f5ace8b3818f704b9ac4c31
DenisSeroshtan <haron_777@mail.ru>
2017-09-07 23:41:27 +0500

    secrion feedback

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -2,5 +2,6 @@
 
 module.exports = [
   "./bower_components/jQuery.mmenu/dist/jquery.mmenu.all.css",
-  "./bower_components/owl.carousel/dist/assets/owl.carousel.css"
+  "./bower_components/owl.carousel/dist/assets/owl.carousel.css",
+  "./bower_components/fotorama/fotorama.css"
 ];
--- source/js/regex.js
+++ source/js/regex.js
@@ -1,8 +1,13 @@
 (function () {
 
-  $('.services-title__top').each(function() {
+  $('.js-h3').each(function() {
     var $this = $(this);
     $this.html($this.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+  });
+
+  $('.js-h2').each(function() {
+    var $this = $(this);
+    $this.html($this.html().replace(/^(\S+)/, '<span>$1</span>'));
   });
 
 })();
